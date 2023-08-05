0d1b58ae80d24f21895aec62b8bae4b5e2582d69
rafaelmoreira.at@hotmail.com <rafaelmoreira.at@hotmail.com>
2023-08-05 02:48:04 -0400

    <Feat> Implementação do navbar e do header do frontend

---
+++ front-end/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+
+const routes: Routes = [
+  {
+    path: '', 
+    component: HeaderComponent,
+    children: [
+
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
